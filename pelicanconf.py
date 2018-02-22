127c584a9e2e39af19dec31e17c0049aaca6cebe
gcman <gcman@users.noreply.github.com>
2018-02-21 21:48:47 -0500

    Added favicon

--- pelicanconf.py
+++ pelicanconf.py
@@ -36,8 +36,8 @@ DEFAULT_DATE_FORMAT = ('%-d %B %Y')
 DEFAULT_CATEGORY = 'misc'
 
 THEME = "../pelican-themes/gcman"
-STATIC_PATHS = ['images', 'figures', 'extra/CNAME', 'extra/404.html']
-EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},'extra/404.html': {'path': '404.html'},}
+STATIC_PATHS = ['images', 'figures', 'extra/CNAME', 'extra/404.html', 'extra/favicon.ico']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},'extra/404.html': {'path': '404.html'},'extra/favicon.ico': {'path': 'favicon.ico'}}
 
 READERS = {'html': None}
 
