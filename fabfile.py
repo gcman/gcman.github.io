87fd82aa4a5667e0883efc1cb48ad6674f478cba
gcman <gcman@users.noreply.github.com>
2018-03-19 12:30:31 -0400

    MD hosting should finally be working now

--- fabfile.py
+++ fabfile.py
@@ -15,6 +15,8 @@ from shutil import copyfile
 from git import Repo
 from pelican.server import ComplexHTTPRequestHandler
 from contextlib import contextmanager
+import re
+import fileinput
 
 ROOT = os.getcwd()
 
@@ -153,6 +155,10 @@ def make_source():
 				with open(file, "r") as f:
 					data = markdown2.markdown(f.read(), extras=["metadata"]).metadata
 					slug = data["slug"].lower()
+					if "status" in data:
+						status = data["status"].lower()
+					else:
+						status = None
 				out = path.join(path.dirname(rootdir),"output")
 				pdfdir = path.join(out,"pdf")
 				rawdir = path.join(out,"raw")
@@ -161,11 +167,26 @@ def make_source():
 				MD = path.join(rawdir,slug+".md")
 				if file in diff or not path.isfile(MD):
 					print("Copying {}".format(file))
-					copyfile(file,path.join(MD))
-				if file in diff or path.join(ROOT, "/content/extra/header.tex") in diff or not path.isfile(path.join(pdfdir,slug+".pdf")):
-					print("Building PDF from {}".format(file))
-					shell("pandoc extra/default.yaml -H extra/header.tex --template extra/template.tex --listings "
-						+ file + " -o " + "../output/pdf/" + slug + ".pdf")
+					with open(file, "r") as f:
+						lines = [line for line in f]
+						to_replace = []
+						s = "---\n"
+						for i,line in enumerate(lines):
+							if s in line:
+								to_replace.append(i)
+							if len(to_replace) == 2:
+								break
+					with open(MD, "w", encoding="utf-8") as f:
+						for i,line in enumerate(lines):
+							if i in to_replace:
+								f.write(line.replace("---",u"\u2010\u2010\u2010"))
+							else:
+								f.write(line)
+				if status != "draft":
+					if file in diff or path.join(ROOT, "/content/extra/header.tex") in diff or not path.isfile(path.join(pdfdir,slug+".pdf")):
+						print("Building PDF from {}".format(file))
+						shell("pandoc extra/default.yaml -H extra/header.tex --template extra/template.tex --listings "
+							+ file + " -o " + "../output/pdf/" + slug + ".pdf")
 	os.chdir(ROOT)
 
 def del_tex2pdf():
