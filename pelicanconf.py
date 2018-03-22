fe8b6372ed049c998ec1f3510868266b93b5db7f
gcman <gcman@users.noreply.github.com>
2018-03-21 21:00:01 -0400

    Added even/odd 404 page

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,7 +5,7 @@ import markdown
 import figureAltCaption
 
 AUTHOR = 'Gautam Manohar'
-SITENAME = "Gautam Manohar"
+SITENAME = 'Gautam Manohar'
 SITEURL = 'https://gautammanohar.com/blog'
 
 GOOGLE_ANALYTICS = 'UA-115904815-1'
