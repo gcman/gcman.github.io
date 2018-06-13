699143a1b982424cf6acc1d8a877cd36a142b8d1
Gautam Manohar <gcmanohar001@gmail.com>
2018-06-12 22:49:38 -0400

    Euler 23

--- content/euler/solutions/21-Amicable-Numbers/main.py
+++ content/euler/solutions/21-Amicable-Numbers/main.py
@@ -1,8 +1,6 @@
-from math import sqrt
-
 def divisor_sum(n):
 	s = 0
-	for i in range(1,int(sqrt(n))+1):
+	for i in range(1,int(n**0.5)+1):
 		if n % i == 0:
 			s += i
 			if i != n//i:
--- content/euler/solutions/22-Names-Scores/main.py
+++ content/euler/solutions/22-Names-Scores/main.py
@@ -1,23 +1,12 @@
-from math import sqrt
+ALPH = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-def divisor_sum(n):
-	s = 0
-	for i in range(1,int(sqrt(n))+1):
-		if n % i == 0:
-			s += i
-			if i != n//i:
-				s += n//i
-	return s - n
+def score(s):
+	return sum([ALPH.index(x) for x in s]) + len(s)
 
-MAX = int(1e5)
-AMICABLE = set()
-for x in range(1,MAX+1):
-	friend = divisor_sum(x)
-	if x == divisor_sum(friend) and x != friend:
-		AMICABLE.add(x)
-
-T = int(input())
-for _ in range(T):
-	N = int(input())
-	S = sum([x for x in AMICABLE if x < N])
-	print(S)
+N = int(input())
+names = sorted([input().strip() for _ in range(N)])
+Q = int(input())
+for _ in range(Q):
+	name = input().strip()
+	ans = score(name)*(names.index(name) + 1)
+	print(ans)
