344a3e0fca58be82dd68aed7da01505373e30da2
gcman <gcmanohar001@gmail.com>
2018-06-24 09:15:46 -0400

    Euler 32

--- content/euler/solutions/32-Pandigital-Products/main.py
+++ content/euler/solutions/32-Pandigital-Products/main.py
@@ -1,17 +1,19 @@
-def perms(s):        
-	if len(s) == 1:
-		return [s]
-	out = []
-	for i,v in enumerate(s):
-		out += [v+p for p in perms(s[:i]+s[i+1:])]
-	return out
+from itertools import permutations
 
 N = int(input())
-PANDIGITAL = perms("".join(map(str,range(1,N+1))))
+
+# Get all pandigital numbers
+PANDIGITAL = permutations("".join(map(str,range(1,N+1))))
+# Use set to avoid duplicates
 PROD = set()
-for x in PANDIGITAL:
+for perm in PANDIGITAL:
+	# Split into 3 parts
+	# Make sure end of second part is after beginning
+	# j > i
 	for i in range(1,N):
 		for j in range(i+1,N):
+			x = "".join(perm)
+			# Check if three parts in order are valid
 			if int(x[:i]) * int(x[i:j]) == int(x[j:]):
 				PROD.add(int(x[j:]))
 print(sum(PROD))
