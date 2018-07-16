b210b1eda0412a1a73357312dfb528a61c33a0ce
gcman <gcmanohar001@gmail.com>
2018-07-16 14:36:33 -0600

    Euler 47 and 48

--- content/euler/solutions/47-Distinct-Primes-Factors/main.py
+++ content/euler/solutions/47-Distinct-Primes-Factors/main.py
@@ -1,4 +1,7 @@
-def distinct_prime_factors(n):
+# Modified Sieve of Eratosthenes
+# Returns no. of distinct prime factors
+# For all numbers up to n
+def omega(n):
 	sieve = [0] * (n + 1)
 	for p in range(2, n + 1):
 		if sieve[p] == 0:
@@ -6,8 +9,10 @@ def distinct_prime_factors(n):
 				sieve[i] += 1
 	return sieve
 
+# Find all consec. sets of size k
+# k distinct prime factors each
 def consecutive(n,k):
-	FACTORS = distinct_prime_factors(n+k-1)
+	FACTORS = omega(n+k-1)
 	out = []
 	for i in range(n+1):
 		if all([FACTORS[i+j] == k for j in range(k)]):
